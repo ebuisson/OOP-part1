85a50e599b28557898dd17cd04b4c1ede770c414
Elias Storme <elias.storme@kuleuven.be>
2023-02-16 15:59:00 +0100

    Added test project, corrected .project files

---
+++ TEST/src/breakout/TaskBTestSuite.java
@@ -0,0 +1,15 @@
+package breakout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class TaskBTestSuite {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
