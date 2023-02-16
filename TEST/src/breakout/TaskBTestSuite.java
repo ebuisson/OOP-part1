a329a6c27e9bce50cfae207bac549a1e34988d11
Elias Storme <elias.storme@kuleuven.be>
2023-02-16 17:34:39 +0100

    Added test suite template

--- TEST/src/breakout/TaskBTestSuite.java
+++ TEST/src/breakout/TaskBTestSuite.java
@@ -1,15 +1,83 @@
 package breakout;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Color;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import breakout.BlockState;
+import breakout.BreakoutState;
+import breakout.GameMap;
+import breakout.PaddleState;
+import breakout.utils.*;
 
-class TaskBTestSuite {
+/**
+ * Those tests should fail on the provided bad implementaiton, succeed on the model solution.
+ */
+class TaskBTestSuiteTemplate {
+	
 
-	@Test
-	void test() {
-		assertTrue(true);
+	private Point BR;
+	private BlockState ablock;
+	private BlockState[] someblocks;
+	private Ball aball;
+	private Ball[] someballs;
+	private PaddleState apad;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
 	}
 
+	@BeforeEach
+	/**
+	 * some basic values to help us write tests
+	 */
+	void setUp() {
+		BR = new Point(Constants.WIDTH, Constants.HEIGHT);
+		
+		ablock = new BlockState(
+				new Rect( Constants.ORIGIN, new Point(Constants.BLOCK_WIDTH,Constants.BLOCK_HEIGHT)) );
+		someblocks = new BlockState[] { ablock };
+		
+		apad = new PaddleState(
+				new Point( Constants.WIDTH / 2, (3 * Constants.HEIGHT) / 4),
+				Constants.TYPICAL_PADDLE_COLORS());
+		
+		aball =
+				new Ball(
+					new Circle(
+						new Point(BR.getX() / 2 , Constants.HEIGHT / 2)
+						, Constants.INIT_BALL_DIAMETER)
+					, Constants.INIT_BALL_VELOCITY);
+		someballs = new Ball[] { aball };
+	}
+
+//	@Test
+//	void exampleTest1() {
+//		assertThrows( IllegalArgumentException.class,
+//				() -> insertcodehere );
+//      assertTrue ( 1 == 1 );
+//      assertEquals( expected, actual );
+//      assertNotEquals( unexpected, actual );
+//      assertNotSame( unexpected, actual );
+//      abreakoutState.tickDuring( 200 );
+//	}
+	
+	@Test
+	void dummyTest() {
+		
+		assertEquals( 35, aball.dummy() );
+	}
+	
+
+
+	
 }
+
