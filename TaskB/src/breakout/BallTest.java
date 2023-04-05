cfceddef1b8efddbdcd97241239e1d49c4b99022
ebuisson <emilie.buisson@student.kuleuven.be>
2023-04-05 14:50:04 +0200

    moved junit test cases from task A to task B

--- TaskB/src/breakout/BallTest.java
+++ TaskB/src/breakout/BallTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class BreakoutStateTest {
+class BallTest {
 
 	@Test
 	void test() {
---
+++ TaskB/src/breakout/BreakoutStateTest.java
@@ -0,0 +1,21 @@
+package breakout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BreakoutStateTest {
+
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
