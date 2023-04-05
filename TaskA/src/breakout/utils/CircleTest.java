7ce63fa3918b9aa324710ee3ade7879ec1c21cc6
ebuisson <emilie.buisson@student.kuleuven.be>
2023-04-05 10:22:51 +0200

    Added test cases for the classes edited in Task A.

---
+++ TaskA/src/breakout/utils/CircleTest.java
@@ -0,0 +1,14 @@
+package breakout.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CircleTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ TaskA/src/breakout/utils/VectorTest.java
@@ -0,0 +1,14 @@
+package breakout.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class VectorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
